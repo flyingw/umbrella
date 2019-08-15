78652f0b6792c8edbd840be2d87d20f299c454bc
Andrii Zadorozhnii <doxtop@synrc.com>
2019-08-16 00:47:56 +0300

    rm bitcoin lib deps

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,11 @@ use structopt::StructOpt;
 use bch::network::Network;
 use bch::messages::{Message, Version, NODE_BITCOIN_CASH, PROTOCOL_VERSION, Tx, TxIn, OutPoint, TxOut};
 use bch::peer::Peer;
-use bch::util::{secs_since,hash160, Amount, Hash256, Units};
+use bch::util::{secs_since,Amount, Hash256, Units};
 use bch::util::rx::Observable;
 use std::time::UNIX_EPOCH;
 use std::net::{IpAddr, Ipv4Addr};
 
-use bch::script::op_codes::*;
 use bch::script::Script;
 
 pub mod conf;
@@ -48,19 +47,19 @@ fn main() {
     
     let ins = vec![TxIn{
         prev_output: OutPoint {
-            hash: Hash256::decode("7375c75c2b24e6e51a71f912ed61e937e70c7fcf7d1008229b87e26555e23e8a").unwrap(),
-            index: 1,
+            hash: Hash256::decode("20a396c97db6158b11b4437a337279849ccfdb92b6e339b069cf06c9039c272c").unwrap(),
+            index: 0,
         },
         sequence: 4294967295,
-        sig_script: Script(hex::decode("47304402200a7866430b70da3a8abf03b5e3711abcc5c926ac05ff3f274dd1c0ad317cc74d022050bcf0e4a0dc424644d4f7d90360cfdcca845e4dbdaf0ae0484b363f1b157d5c41210262cb2c04df5b31574df9568aa9a5c8e02b5b82fa4813e69a1bc7ac15d2042d93").unwrap()),
+        sig_script: Script(hex::decode("483045022100c2c11e494efc98caf485a09d7166dfc5e1c1e8cfe2cf85d391ab59f2c360b90b022021ea942f67cb9f694108ed3798b2aec00e7c2bb328e62927473f4990b76e746d412103c0080bf04a431c8082fe9f67477f826e14406f20482759b59905a5035abec2f5").unwrap()),
     }];
 
     let outs = vec![TxOut{
-        amount: Amount::from(1., Units::Bch),
-        pk_script: Script(hex::decode("76a9146e3c0d8fe75ef9c8dcd49be1c3d3f4d790ed8ce388ac").unwrap()),
+        amount: Amount::from(0.3, Units::Bch),
+        pk_script: Script(hex::decode("76a914926d53aeedc28d97d3819bf940ee303b94b9002c88ac").unwrap()),
     }, TxOut{
-        amount: Amount::from(7.9998, Units::Bch),
-        pk_script: Script(hex::decode("76a91478854a9b95b7672d9011ed690f8677ee5bad8b0288ac").unwrap()),
+        amount: Amount::from(0.6999, Units::Bch),
+        pk_script: Script(hex::decode("76a914e5216a352682217094f5d86b8d00188b02d1791e88ac").unwrap()),
     }];
 
     let tx = Tx {
