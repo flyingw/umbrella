3d838a44aec758e7ca78bc5f1127ad54786f7d04
Andrii Zadorozhnii <andrii.zadorozhnii@intellectsoft.net>
2019-09-18 14:48:49 +0200

    header without rlp

--- src/messages/hello.rs
+++ src/messages/hello.rs
@@ -40,23 +40,25 @@ impl <'a> Serializable<Hello<'a>> for Hello<'a>{
             .append(&u_public_key);
         
         let payload: &[u8] = &rlp.out();
-
-        const HEADER_LEN: usize = 16;
-		let mut header = RlpStream::new();
-		let len = payload.len();
-		if len > MAX_PAYLOAD_SIZE {
+        let len = payload.len();
+        if len > MAX_PAYLOAD_SIZE {
 			panic!("OversizedPacket {}", len);
 		}
 
-		header.append_raw(&[(len >> 16) as u8, (len >> 8) as u8, len as u8], 1);
-		header.append_raw(&[0xc2u8, 0x80u8, 0x80u8], 1);
-		let padding = (16 - (len % 16)) % 16;
+        //
+        const HEADER_LEN: usize = 16;
+        let mut header = [0u8;HEADER_LEN];
+        let (sz, rest) = header.split_at_mut(3);
+        sz.copy_from_slice(&[(len >> 16) as u8, (len >> 8) as u8, len as u8]);
+        let (magic, _) = rest.split_at_mut(3);
+        magic.copy_from_slice(&[0xc2u8, 0x80u8, 0x80u8]);
+        
+        self.connection.encoder.encrypt(&mut header).unwrap();
 
-		let mut packet: Vec<u8> = vec![0u8; 16 + 16 + len + padding + 16];
-		let mut header = header.out();
-		header.resize(HEADER_LEN, 0u8);
-		&mut packet[..HEADER_LEN].copy_from_slice(&mut header);
-		self.connection.encoder.encrypt(&mut packet[..HEADER_LEN]).unwrap();
+        writer.write_all(&header)?;
+
+        let padding = (16 - (len % 16)) % 16;
+        let mut packet: Vec<u8> = vec![0u8; 16 + 16 + len + padding + 16];
 
         // connection drops previous shit here
 		let mut prev = Hash128::default();
@@ -74,7 +76,7 @@ impl <'a> Serializable<Hello<'a>> for Hello<'a>{
 
         // to previous mac we've add fucking xor of header
         // and update tha mac 
-		enc = enc ^ Hash128::from_slice(&packet[..HEADER_LEN]);
+		enc = enc ^ Hash128::from_slice(&header);
 		self.connection.egress_mac.update(enc.as_bytes());
 
         // write that shit next to packet header
@@ -88,30 +90,38 @@ impl <'a> Serializable<Hello<'a>> for Hello<'a>{
 		if padding != 0 {
 			self.connection.encoder.encrypt(&mut packet[(32 + len)..(32 + len + padding)]).unwrap();
 		}
+
+        // header +
+        writer.write_all(&packet[HEADER_LEN..32])?;
+        
         // update mac with new fucking data
 		self.connection.egress_mac.update(&packet[32..(32 + len + padding)]);
+        writer.write_all(&packet[32..(32 + len + padding)])?;
 
-        //
+        // something we've in mac
         let mut prev = Hash128::default();
-		self.connection.egress_mac.clone().finalize(prev.as_bytes_mut());
+        self.connection.egress_mac.clone().finalize(prev.as_bytes_mut());
 
-        // write it as previous shit
+        debug!("prev bytes {:?}", prev.as_bytes());
+
+        // xor previous bytes with encrypted of this previous bytes
 		let mut enc = Hash128::default();
 		&mut enc[..].copy_from_slice(prev.as_bytes());
-		let mac_encoder = AesEcb256::new(&self.connection.mac_encoder_key.as_bytes()).unwrap();
+        let mac_encoder = AesEcb256::new(&self.connection.mac_encoder_key.as_bytes()).unwrap();
 		mac_encoder.encrypt(enc.as_bytes_mut()).unwrap();
-        // do all the useless shit with that and just use its as an output
 
-
-		enc = enc ^ prev;
+        debug!("prev enc {:?}", enc.as_bytes());
+        debug!("    prev {:?}", prev.as_bytes());
+        debug!("     xor {:?}", (enc ^ prev).as_bytes());
 		
-        self.connection.egress_mac.update(enc.as_bytes());
-        // egress will be updated with whole package here, but what aboit finilize of clone?
+        // egress mac update agani with xored encrypted shit
+        self.connection.egress_mac.update((enc ^ prev).as_bytes());
 
-		self.connection.egress_mac.clone().finalize(&mut packet[(32 + len + padding)..]);
+        let mut b = [0;16];
+        self.connection.egress_mac.clone().finalize(&mut b);
 
-        // write final doubly encrypted package?
-        writer.write_all(packet.as_ref())
+        debug!("last 16 {:?}", b);
+        writer.write_all(&b)
     }
 
 }
