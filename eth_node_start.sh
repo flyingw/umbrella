60482e918d3e284b98acfbbe7ce07f3c7d293b81
Andrii Zadorozhnii <andrii.zadorozhnii@intellectsoft.net>
2019-09-20 13:35:49 +0200

    pass

--- eth_node_start.sh
+++ eth_node_start.sh
@@ -7,5 +7,6 @@ VERBOSITY=7
 PORT=30301
 NETWORK_ID="123"
 pub1="f3f1125d9445748dea24b8e087a6419539c0e816" # cut from json in settings
+PASS="test"
 
 printf ${PASS}'\n' | geth --datadir $DATA_DIR -verbosity $VERBOSITY --port $PORT --networkid $NETWORK_ID --unlock $pub1 --mine --nodiscover
