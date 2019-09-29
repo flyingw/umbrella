f195ad48b2d3212d42a77dcbce581ea51fa1b7bc
Andrii Stasiuk <shmishleniy@gmail.com>
2019-09-29 04:58:24 +0300

    remove parity-crypto

--- src/ecies.rs
+++ src/ecies.rs
@@ -1,9 +1,17 @@
-use parity_crypto::{aes, digest, hmac, is_equal};
 use rand;
 use rand::RngCore;
 use secp256k1::{self, ecdh};
 use secp256k1::{Secp256k1, SecretKey, PublicKey};
 use crate::keys::{public_to_slice, slice_to_public};
+use aes_ctr::Aes128Ctr;
+use aes_ctr::stream_cipher::NewStreamCipher;
+use aes_ctr::stream_cipher::SyncStreamCipher;
+use aes::block_cipher_trait::generic_array::GenericArray;
+use ring::digest::{digest, SHA256};
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
+
+type HmacSha256 = Hmac<Sha256>;
 
 fn hash(output: &mut [u8], x: &[u8], _y: &[u8]) -> i32 {
   kdf(&x, &[0u8; 0], output);
@@ -21,8 +29,8 @@ pub fn encrypt(public_key: &PublicKey, auth_data: &[u8], plain: &[u8]) -> Result
 
   let key = ecdh::SharedSecret::new_with_hash(&public_key, &secret_key_rand, &mut hash);
 
-  let ekey = &key[0..16];
-  let mkey = hmac::SigKey::sha256(&digest::sha256(&key[16..32]));
+  let ekey = &key[0..16];;
+  let mkey = digest(&SHA256, &key[16..32]);
 
   let mut msg = vec![0u8; 1 + 64 + 16 + plain.len() + 32];
   msg[0] = 0x04u8;
@@ -34,15 +42,17 @@ pub fn encrypt(public_key: &PublicKey, auth_data: &[u8], plain: &[u8]) -> Result
     msgd[64..80].copy_from_slice(&iv);
     {
       let cipher = &mut msgd[(64 + 16)..(64 + 16 + plain.len())];
-      aes::encrypt_128_ctr(ekey, &iv, plain, cipher).unwrap();
+      let mut enc = Aes128Ctr::new(GenericArray::from_slice(ekey), GenericArray::from_slice(&iv));
+      cipher[..plain.len()].copy_from_slice(plain);
+	    enc.try_apply_keystream(cipher).unwrap();
     }
-    let mut hmac = hmac::Signer::with(&mkey);
+    let mut hmac = HmacSha256::new_varkey(mkey.as_ref()).unwrap();
     {
       let cipher_iv = &msgd[64..(64 + 16 + plain.len())];
-      hmac.update(cipher_iv);
+      hmac.input(cipher_iv);
     }
-    hmac.update(auth_data);
-    let sig = hmac.sign();
+    hmac.input(auth_data);
+    let sig = hmac.result().code();;
     msgd[(64 + 16 + plain.len())..].copy_from_slice(&sig);
   }
   Ok(msg)
@@ -61,7 +71,7 @@ pub fn decrypt(secret_key: &SecretKey, auth_data: &[u8], encrypted: &[u8]) -> Re
   let key = ecdh::SharedSecret::new_with_hash(&public_key, &secret_key, &mut hash);
   
   let ekey = &key[0..16];
-  let mkey = hmac::SigKey::sha256(&digest::sha256(&key[16..32]));
+  let mkey = digest(&SHA256, &key[16..32]);
 
   let clen = encrypted.len() - meta_len;
   let cipher_with_iv = &e[64..(64+16+clen)];
@@ -70,17 +80,19 @@ pub fn decrypt(secret_key: &SecretKey, auth_data: &[u8], encrypted: &[u8]) -> Re
   let msg_mac = &e[(64+16+clen)..];
 
   // Verify tag
-  let mut hmac = hmac::Signer::with(&mkey);
-  hmac.update(cipher_with_iv);
-  hmac.update(auth_data);
-  let mac = hmac.sign();
+  let mut hmac = HmacSha256::new_varkey(mkey.as_ref()).unwrap();
+  hmac.input(cipher_with_iv);
+  hmac.input(auth_data);
+  let mac = hmac.result().code();;
 
-  if !is_equal(&mac.as_ref()[..], msg_mac) {
+  if !(&mac.as_ref()[..] == msg_mac) {
     panic!("decrypt error=InvalidMessage, wrong mac");
   }
 
   let mut msg = vec![0u8; clen];
-  aes::decrypt_128_ctr(ekey, cipher_iv, cipher_no_iv, &mut msg[..]).unwrap();
+  let mut enc = aes_ctr::Aes128Ctr::new(GenericArray::from_slice(ekey), GenericArray::from_slice(cipher_iv));
+	msg[..cipher_no_iv.len()].copy_from_slice(cipher_no_iv);
+	enc.try_apply_keystream(&mut msg).unwrap();
   Ok(msg)
 }
 
@@ -91,13 +103,13 @@ fn kdf(shared: &[u8], s1: &[u8], dest: &mut [u8]) {
   let mut ctr = 1u32;
   let mut written = 0usize;
   while written < dest.len() {
-    let mut hasher = digest::Hasher::sha256();
     let ctrs = [(ctr >> 24) as u8, (ctr >> 16) as u8, (ctr >> 8) as u8, ctr as u8];
-    hasher.update(&ctrs);
-    hasher.update(&shared[..]);
-    hasher.update(s1);
-    let d = hasher.finish();
-    &mut dest[written..(written + 32)].copy_from_slice(&d);
+    let mut buf: Vec<u8> = vec![];
+    buf.extend(&ctrs);
+    buf.extend(&shared[..]);
+    buf.extend(s1);
+    let d = digest(&SHA256, &buf);
+    &mut dest[written..(written + 32)].copy_from_slice(d.as_ref());
     written += 32;
     ctr += 1;
   }
