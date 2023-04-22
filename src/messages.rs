caccec137d4c83bfb3081824331e49d5c2b31e09
tellnobody1 <6303577+tellnobody1@users.noreply.github.com>
2023-04-22 12:03:04 +0300

    revert messages

--- src/messages.rs
+++ src/messages.rs
@@ -15,13 +15,11 @@ mod node_key;
 mod hello;
 mod status;
 mod tx2;
-mod bsv;
 pub mod fee_filter;
 
 pub use version::{Version,NODE_NONE,PROTOCOL_VERSION};
 pub use tx::Tx;
 pub use tx2::Tx2;
-pub use bsv::{TxBsv, UnspentBsv, Output};
 pub use tx_in::TxIn;
 pub use tx_out::TxOut;
 pub use out_point::OutPoint;
